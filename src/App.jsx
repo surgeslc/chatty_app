fe7533df1a484614bef3bab392d3301224fe78ff
Lawrence Surges <surgeslc@gmail.com>
2017-07-26 03:49:30 +0000

    Completed .../w5d2/activities/368 Step 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,29 @@ import NavBar from "./NavBar.jsx";
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"},
+        messages: [
+          {
+            username: "Bob",
+          content: "Has anyone seen my marbles?",
+          },
+          {
+            username: "Anonymous",
+            content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+          }
+        ],
+      loading: false};
+  }
   render() {
-    console.log("Rendering <App/>");
+    //console.log("Rendering <App/>");
     return (
       <div>
         <NavBar />
         <MessageList />
-        <ChatBar />
+        <ChatBar { ...this.state.currentUser } />
       </div>
     );
   }
